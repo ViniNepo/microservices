43706e5923b2afdc32da8ddca4381583b9ab43aa
Vinicius Duarte Mendes Nepomuceno <vinicius.nepomuceno1@ibm.com>
2022-03-14 15:29:50 -0300

    hr-worker implementado

---
+++ hr-worker/src/main/java/com/vinicius/cursos/hrworker/repository/WorkerRepository.java
@@ -0,0 +1,10 @@
+package com.vinicius.cursos.hrworker.repository;
+
+import com.vinicius.cursos.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
